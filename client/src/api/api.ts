2475047e01edac49be144de569d8d49393b845aa
shiv4006 <shivamkumar404090@gmail.com>
2026-01-22 23:56:53 +0530

    added interface for auth routes

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,11 +1,23 @@
 import api from './axios';
 
 /* AUTH */
-export const registerUser = (data) => api.post("/auth/register", data);
-export const loginUser = (data) => api.post("/auth/login", data);
+export interface RegisterUserData {
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+}
+export interface LoginUserData {
+  email: string;
+  password: string;
+}
+
+export const registerUser = (data: RegisterUserData) => api.post("/auth/register", data);
+
+export const loginUser = (data: LoginUserData) => api.post("/auth/login", data);
 export const logoutUser = () => api.post("/auth/logout");
 export const getProfile = () => api.get("/auth/profile");
 
 /* NEWSLETTER */
-export const subscribeNewsletter = async (email) =>
+export const subscribeNewsletter = async (email: string) =>
   await api.post("/newsletter/subscribe", { email });
